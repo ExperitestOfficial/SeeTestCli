81396e1ba00d95dad8192f176eed05350d0f267a
DESKTOP-GQ681HR\guyar <guy@experitest.com>
2019-12-03 13:14:28 +0200

    support multiple device ids in move

--- src/main/java/com/experitest/cloud/cli/GroupsMove.java
+++ src/main/java/com/experitest/cloud/cli/GroupsMove.java
@@ -12,59 +12,62 @@ import java.util.List;
         name = "move"
 )
 public class GroupsMove implements Runnable{
-    @CommandLine.Option(names = {"-id"}, description = {"Device ID/Name"}, required = true)
-    private String id;
+    @CommandLine.Option(names = {"-id"}, description = {"Device ID/Name list (comma separated)"}, required = true)
+    private String ids;
     @CommandLine.Option(names = {"-r"}, description = {"Remove from group name"}, required = false)
     private String from;
     @CommandLine.Option(names = {"-a"}, description = {"Add to group name"}, required = false)
     private String to;
     public void run(){
         try (Cloud cloud = CloudUtils.getCloud()) {
-            int deviceId = -1;
-            try{
-                deviceId = Integer.parseInt(id);
-            } catch (Exception ignore){
-                List<Device> devices = cloud.devices().get();
-                for(Device d: devices){
-                    if(id.equalsIgnoreCase(d.getName())){
-                        deviceId = d.getId();
-                        break;
-                    }
-                }
-            }
-            if(deviceId == -1){
-                throw new RuntimeException("Cannot find device");
-            }
             List<DeviceGroup> groups = cloud.deviceGroups().getAll();
-            if(from != null){
-                DeviceGroup group = null;
-                for(DeviceGroup g: groups){
-                    if(from.equalsIgnoreCase(g.getName())){
-                        group = g;
-                        break;
+            List<Device> devices = cloud.devices().get();
+            String[] values = ids.split(",");
+            for(String value: values) {
+                int deviceId = -1;
+                try {
+                    deviceId = Integer.parseInt(value);
+                } catch (Exception ignore) {
+                    for (Device d : devices) {
+                        if (value.equalsIgnoreCase(d.getName())) {
+                            deviceId = d.getId();
+                            break;
+                        }
                     }
                 }
-                if(group == null){
-                    throw new RuntimeException("Group " + from + " wasn't found");
+                if (deviceId == -1) {
+                    throw new RuntimeException("Cannot find device");
                 }
-                Device d = new Device();
-                d.setId(deviceId);
-                cloud.deviceGroups().removeDeviceFromGroup(d, group);
-            }
-            if(to != null){
-                DeviceGroup group = null;
-                for(DeviceGroup g: groups){
-                    if(to.equalsIgnoreCase(g.getName())){
-                        group = g;
-                        break;
+                if (from != null) {
+                    DeviceGroup group = null;
+                    for (DeviceGroup g : groups) {
+                        if (from.equalsIgnoreCase(g.getName())) {
+                            group = g;
+                            break;
+                        }
                     }
+                    if (group == null) {
+                        throw new RuntimeException("Group " + from + " wasn't found");
+                    }
+                    Device d = new Device();
+                    d.setId(deviceId);
+                    cloud.deviceGroups().removeDeviceFromGroup(d, group);
                 }
-                if(group == null){
-                    throw new RuntimeException("Group " + to + " wasn't found");
+                if (to != null) {
+                    DeviceGroup group = null;
+                    for (DeviceGroup g : groups) {
+                        if (to.equalsIgnoreCase(g.getName())) {
+                            group = g;
+                            break;
+                        }
+                    }
+                    if (group == null) {
+                        throw new RuntimeException("Group " + to + " wasn't found");
+                    }
+                    Device d = new Device();
+                    d.setId(deviceId);
+                    cloud.deviceGroups().assignDeviceToGroup(d, group);
                 }
-                Device d = new Device();
-                d.setId(deviceId);
-                cloud.deviceGroups().assignDeviceToGroup(d, group);
             }
             System.out.println("Done!");
 

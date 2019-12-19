bb7169d9b96750375e8529658a16a12cb1b3d10b
DESKTOP-GQ681HR\guyar <guy@experitest.com>
2019-12-19 13:28:56 +0200

    User list improvements

--- src/main/java/com/experitest/cloud/cli/UsersList.java
+++ src/main/java/com/experitest/cloud/cli/UsersList.java
@@ -12,6 +12,8 @@ import java.util.List;
         name = "list"
 )
 public class UsersList implements Runnable{
+    @CommandLine.Option(names = {"-b"}, description = {""}, required = false)
+    private boolean b;
     @CommandLine.Option(names = {"-f"}, description = {"Comma separated list"}, required = false)
     private String filter;
     @CommandLine.Option(names = {"-csv"}, description = {"Comma separated list"}, required = false)
@@ -19,18 +21,35 @@ public class UsersList implements Runnable{
     public void run(){
         try (Cloud cloud = CloudUtils.getCloud()) {
             List<User> users = cloud.users().get();
-            for(User u: users) {
-                if(u.getProjects() != null && u.getProjects().length > 0){
+            for (User u : users) {
+                if (u.getProjects() != null && u.getProjects().length > 0) {
                     u.setRole(u.getProjects()[0].getProjectRole());
                     StringBuffer buf = new StringBuffer();
-                    for(Project p : u.getProjects()){
+                    for (Project p : u.getProjects()) {
                         buf.append(p.getName());
                         buf.append(",");
                     }
-                   u.setProjectString(buf.substring(0, buf.length() - 1));
+                    u.setProjectString(buf.substring(0, buf.length() - 1));
                 }
             }
-            CloudUtils.printAsTable(filter, csv, users, "id", "username", "email", "role", "projectstring", "lastauthentication");
+            if (b) {
+                for(User user: users){
+                    String[] userStatus = user.getData("userstatus");
+                    StringBuilder buf = new StringBuilder();
+                    buf.append(user.getEmail());
+                    buf.append("|");
+                    buf.append(user.getUsername());
+                    buf.append("|");
+                    buf.append(user.getRole());
+                    buf.append("||||||");
+                    buf.append(user.getRole());
+                    buf.append("|||");
+                    buf.append("user".equalsIgnoreCase(user.getRole())? "N": "Y");
+                    System.out.println(buf.toString());
+                }
+            } else {
+                CloudUtils.printAsTable(filter, csv, users, "id", "username", "email", "role", "projectstring", "lastauthentication", "userstatus");
+            }
         } catch (IOException ex){
             ex.printStackTrace();
             throw new RuntimeException("Fail to init cloud connection");
